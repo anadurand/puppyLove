80b7b00e92a5e0aed09dc83db81e0435a07777cf
Ana Durand <ana.dc1004@gmail.com>
2017-07-12 13:22:04 -0500

    heroku

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
+var lastScrollTop = 0;
+//cambiar el estilo del header cuando avanza el scroll
+window.addEventListener("scroll", function(){
+   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+   if (currentScroll > 600){
+     $('.header').addClass("header__change");
+     $('.logo').addClass("logo__change");
+
+   }
+   if(currentScroll<=600){
+     $('.header').removeClass("header__change");
+     $('.logo').removeClass("logo__change");
+
+   }
+   lastScrollTop = currentScroll;
+}, false);
+
+
 $(document).ready(function(){
       $('.parallax').parallax();
+
+      $(".button-collapse").sideNav({
+        menuWidth: 300,
+        edge: 'right'
+      });
+      $('.close').on('click',(e)=>{
+        $('#slide-out').sideNav('hide');
+      });
+
+
 });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname));
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
